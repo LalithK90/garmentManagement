48051a4f80b8cb7bb5c066d7264f7729f91392dd
Lositha <asakahatapitiya@gmail.com>
2019-09-18 20:18:49 +0530

    fabric add

--- src/main/java/lk/css/garmentManagement/config/MvcConfig.java
+++ src/main/java/lk/css/garmentManagement/config/MvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -27,20 +26,7 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/login")
                 .setViewName("login/login");
         registry.addViewController("/mainwindow")
-                .setViewName("mainwindow");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //can be impliment like folowing also
-        //registry.addResourceHandler("/resources/**").addResourceLocations("file:/resources/upload/");
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations("classpath:/static/img/");
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/css/");
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/");
-
+                .setViewName("mainWindow");
     }
 
     //Bean config
